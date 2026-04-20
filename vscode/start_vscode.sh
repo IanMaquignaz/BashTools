4995afcf31f85a0355e8a0bbf3257884aaa66075
Ian Maquignaz <9im14@queensu.ca>
2026-04-20 12:54:27 -0400

    update to start_vscode.sh

--- vscode/start_vscode.sh
+++ vscode/start_vscode.sh
@@ -12,23 +12,31 @@ FP_VSCODE_CLI="$HOME/vscode_x64_cli.tar.gz"
 # Latest vscode CLI release
 URL_VSCODE_CLI='https://update.code.visualstudio.com/latest/cli-linux-x64/stable'
 
-
 # Kill Old vscode server(s)
 tmux kill-session -t $SESSION_NAME
 
-# Delete old version
-rm -f $HOME/code
-
 # Download
-wget -N --content-disposition -O $FP_VSCODE_CLI $URL_VSCODE_CLI 
+wget -N --content-disposition -O $FP_VSCODE_CLI $URL_VSCODE_CLI
 #curl -Lk $URL_VSCODE_CLI --output $FP_VSCODE_CLI
 
-if [ -f "$FP_VSCODE_CLI" ]; then
-    echo "Extracting..."
-    # Extract
-    tar -xvf $FP_VSCODE_CLI --directory $HOME
+if [ $? -ne 0 ]; then
+    echo "Failed to download vscode CLI from $URL_VSCODE_CLI. Skipping download."
 else
-    echo "Failed find tarball at $FP_VSCODE_CLI. Skipping extraction."
+    echo "Download complete."
+    tar -ztvf "$FP_VSCODE_CLI" > /dev/null 2>&1
+    if [ $? -eq 0 -a -f "$FP_VSCODE_CLI" ]; then
+        # Backup old version
+        echo "Backing up old version to $HOME/code_old"
+        rm -f $HOME/code_old
+        mv $HOME/code $HOME/code_old
+
+        # Extract new version
+        echo "Extracting $(tar -xvf $FP_VSCODE_CLI --directory $HOME) to $HOME/code"
+        echo "Done!"
+    else
+        echo "Failed to find or verify the tarball at $FP_VSCODE_CLI. Skipping extraction."
+        echo "Will attempt to run old version if it exists."
+    fi
 fi
 
 # Run
